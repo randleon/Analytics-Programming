ee2a26e09a3f87e917df4ec9434fe84c7342903f
Randy Leon <rleon1@mail.yu.edu>
2019-09-01 10:02:51 -0400

    Create R_Leon_W1_assn.py

---
+++ R_Leon_W1_assn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <https://github.com/rleon207/DAV5400/blob/master/mlb_elo_latest.csv> - selected data set, contains game-by-game Elo ratings and forecasts for only the latest season in the MLB; The data was obtained from 538.com from this link <https://data.fivethirtyeight.com/#mlb-elo> 
+# 
+# As mentioned in previous discussions for not only this class but also Visual Design and Storytelling, I am an avid baseball fan, and more so a fan of the New York Yankees. I may be able to use this data set to compare the numbers when we find out who is the World Series winner in late October, or for a project to showcase the fascination behind Elo rankings. Elo, of course, refers to Alpad Elo, a Hungarian-born, expat, chess prodigy, physicist who devised a statistical system for ranking chess players. 
+# 
+# Currently this data set is part of a larger one that goes as far back as 1871. It goes over statistics for the probability of winning in any given exhibition between two teams, both with and without the variables such as travel/rest time for the away team, and the starting pitchers.
+# 
+# #
+
+# In[ ]:
+
+
+
+
